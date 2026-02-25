c95230ef3a40f28d12b6101b6809682ef4c35c01
tomaz.drnovsek <tomaz.drnovsek@gmail.com>
2026-02-25 13:18:33 +0000

    Added heptic feedback to sessions button

--- App.tsx
+++ App.tsx
@@ -370,6 +370,7 @@ const App: React.FC = () => {
                     className="justify-self-end flex flex-col items-end justify-center leading-tight cursor-pointer group select-none"
                     onClick={() => {
                         audio.playClick();
+                        audio.triggerHaptic('light');
                         setShowArchive(true);
                     }}
                 >
